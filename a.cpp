8163f0211a8bc3afa3c04be9fe93fd71787ca6ac
zeithaum@qq.com <zeithaum@qq.com>
2023-03-08 22:20:00 +0800

    Modified a.cpp

--- a.cpp
+++ a.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 
 int main() {
-
     int a = 1;
     int b = 2;
     cout << a + b << "\n";
+    //Added by ZeitHaum
+    int sum = 0;
+    for(int i = 0;i<=100;i++){
+        sum+=i;
+    }
+    cout<<sum<<endl;
+    //Add ended.
     return 0;
 }
