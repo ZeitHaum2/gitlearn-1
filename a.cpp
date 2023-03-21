f8e8a6fb9c9529d0b168d8e94ae4c9e78c78a91a
ZeitHaum <108731262+ZeitHaum@users.noreply.github.com>
2023-03-21 20:02:13 +0800

    Changed to /

--- a.cpp
+++ a.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int a = 1;
     int b = 2;
-    cout << a + b << "\n";
+    cout << a / b << "\n";
     //Added by ZeitHaum
     int sum = 0;
     for(int i = 0;i<=100;i++){
